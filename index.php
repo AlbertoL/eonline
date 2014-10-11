8ae83c0390b72d88bbb50ddbecb762a482f08ad8
Alberto <espiritudeelias@gmail.com>
2014-10-11 10:21:22 -0300

    maquetación

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>E-OnLine</title>
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/estilos.css"/>
+</head>
+<body>
+	<div class="contenedor">
+	<header>
+		<nav>
+			<h1>E-OnLine</h1>
+			<ul>
+				<li><a href="#">Inicio</a> </li>
+				<li><a href="#">Curso</a> </li>
+				<li><a href="#">¿Quieres ser un instructor?</a>  </li>
+				<li><a href="#">Contacto</a> </li>
+			</ul>
+		</nav>
+	</header>
+	<div class="promo">
+		<div class="circulo"><p>Cursos</p></div>
+		<div class="circulo"><p>Profesores</p></div>
+		<div class="circulo"><p>Capacitación</p></div>
+
+	</div>
+	</div>
+</body>
+</html>
