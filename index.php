c1fb85106341cde83462bedc0f39ea9043eb3d01
Alberto <espiritudeelias@gmail.com>
2014-10-12 02:07:47 -0300

    Maquetación Primera Parte

--- index.php
+++ index.php
@@ -13,18 +13,31 @@
 			<h1>E-OnLine</h1>
 			<ul>
 				<li><a href="#">Inicio</a> </li>
-				<li><a href="#">Curso</a> </li>
-				<li><a href="#">¿Quieres ser un instructor?</a>  </li>
+				<li><a href="#">Cursos</a> </li>
+				<li><a href="#">Instituciones</a>  </li>
 				<li><a href="#">Contacto</a> </li>
 			</ul>
 		</nav>
 	</header>
 	<div class="promo">
-		<div class="circulo"><p>Cursos</p></div>
-		<div class="circulo"><p>Profesores</p></div>
-		<div class="circulo"><p>Capacitación</p></div>
-
+		<div class="des">
+			<h1>Educación OnLine</h1>
+			<p>
+				Plataforma virtual para <span>Instituciones</span>  y <span>Profesores.</span> 
+			</p>
+			<input class="boton" type="button" onclick="location.href='#'" value="Registrate Ahora"/>
+			<input class="boton" type="button" onclick="location.href='#'" value="Ver Planes"/>
+		</div>
+		<div class="dimg">
+			<img src="img/bg.png" width="70%" alt="logo"/>
+		</div>
+	</div>
+	<div class="d_plataforma">
+		Únete y se parte de esta nueva forma de enseñar.
+		Incrementa tus posibilidades de alcanzar a más estudiantes.
 	</div>
 	</div>
+	
+	
 </body>
 </html>
